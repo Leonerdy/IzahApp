d3ca506f587a1068bc136dee44b742ac912d88a1
Anselmo Leonardo teixeira da Silva <enge.leon@gmail.com>
2018-10-02 04:59:37 -0300

    Initial Commit
    Initial Commit

---
+++ IzahApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using IzahApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IzahApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){  
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
